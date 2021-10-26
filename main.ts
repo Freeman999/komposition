8ca1032e488728d98096b4fe801191ab37332f1f
Freeman999 <65241975+Freeman999@users.noreply.github.com>
2021-10-26 17:27:13 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,53 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    bpm_erhalten = receivedNumber
+})
+function Musik (bpm: number) {
+    while (müsig) {
+        music.setTempo(bpm)
+        for (let index = 0; index < 2; index++) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            if (Math.map(input.lightLevel(), 0, 255, 0, 2) == 0) {
+                music.playTone(330, music.beat(BeatFraction.Whole))
+            } else if (Math.map(input.lightLevel(), 0, 255, 0, 2) == 1) {
+                music.playTone(392, music.beat(BeatFraction.Whole))
+            } else if (Math.map(input.lightLevel(), 0, 255, 0, 2) == 2) {
+                music.playTone(262, music.beat(BeatFraction.Whole))
+            }
+            music.playTone(262, music.beat(BeatFraction.Whole))
+        }
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(120)
+    basic.pause(1000)
+    radio.sendString("S")
+})
+input.onButtonPressed(Button.AB, function () {
+    müsig = true
+    bpm_erhalten = 120
+    Musik(bpm_erhalten)
+    basic.pause(15000)
+    müsig = false
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "S") {
+        müsig = true
+        Musik(bpm_erhalten)
+    } else if (receivedString == "E") {
+        müsig = false
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("E")
+})
+let bpm_erhalten = 0
+let müsig = false
+radio.setGroup(28)
+radio.setTransmitPower(4)
+müsig = false
 basic.forever(function () {
-	
+    basic.showNumber(bpm_erhalten)
+    basic.pause(1000)
+    basic.showIcon(IconNames.EigthNote)
 })
