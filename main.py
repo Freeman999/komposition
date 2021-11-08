ae6190530c22dbf051f33fedac7652209371de6f
Freeman999 <65241975+Freeman999@users.noreply.github.com>
2021-11-08 18:38:33 +0100

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,20 +3,6 @@ def on_received_number(receivedNumber):
     bpm_erhalten = receivedNumber
 radio.on_received_number(on_received_number)
 
-def Musik(bpm: number):
-    while müsig:
-        music.set_tempo(bpm)
-        for index in range(2):
-            music.play_tone(262, music.beat(BeatFraction.WHOLE))
-            music.play_tone(262, music.beat(BeatFraction.WHOLE))
-            if Math.round(Math.map(input.light_level(), 0, 255, 0, 2)) == 0:
-                music.play_tone(330, music.beat(BeatFraction.WHOLE))
-            elif Math.round(Math.map(input.light_level(), 0, 255, 0, 2)) == 1:
-                music.play_tone(392, music.beat(BeatFraction.WHOLE))
-            elif Math.round(Math.map(input.light_level(), 0, 255, 0, 2)) == 2:
-                music.play_tone(523, music.beat(BeatFraction.WHOLE))
-            music.play_tone(262, music.beat(BeatFraction.WHOLE))
-
 def on_button_pressed_a():
     radio.send_number(120)
     basic.pause(1000)
@@ -29,14 +15,12 @@ def on_button_pressed_ab():
     debug_mode = True
     müsig = True
     bpm_erhalten = 100
-    Musik(bpm_erhalten)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_received_string(receivedString):
     global müsig
     if receivedString == "S":
         müsig = True
-        Musik(bpm_erhalten)
     elif receivedString == "E":
         müsig = False
 radio.on_received_string(on_received_string)
@@ -55,20 +39,15 @@ radio.set_group(28)
 radio.set_transmit_power(4)
 
 def on_forever():
-    if debug_mode == False:
-        if müsig:
-            basic.show_number(bpm_erhalten)
-            basic.show_icon(IconNames.EIGTH_NOTE)
-        else:
-            basic.show_icon(IconNames.HAPPY)
-    elif debug_mode == True:
-        basic.show_leds("""
-            # . # . #
-            . # # # .
-            # # # # #
-            . # # # .
-            # . # . #
-            """)
-        basic.show_number(Math.round(Math.map(input.light_level(), 0, 255, 0, 2)))
-        basic.pause(1000)
+    while müsig:
+        music.set_tempo(bpm_erhalten)
+        music.play_tone(262, music.beat(BeatFraction.WHOLE))
+        music.play_tone(262, music.beat(BeatFraction.WHOLE))
+        if Math.round(Math.map(input.light_level(), 0, 255, 0, 2)) == 0:
+            music.play_tone(330, music.beat(BeatFraction.WHOLE))
+        elif Math.round(Math.map(input.light_level(), 0, 255, 0, 2)) == 1:
+            music.play_tone(392, music.beat(BeatFraction.WHOLE))
+        elif Math.round(Math.map(input.light_level(), 0, 255, 0, 2)) == 2:
+            music.play_tone(523, music.beat(BeatFraction.WHOLE))
+        music.play_tone(262, music.beat(BeatFraction.WHOLE))
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,23 +1,6 @@
 radio.onReceivedNumber(function (receivedNumber) {
     bpm_erhalten = receivedNumber
 })
-function Musik (bpm: number) {
-    while (müsig) {
-        music.setTempo(bpm)
-        for (let index = 0; index < 2; index++) {
-            music.playTone(262, music.beat(BeatFraction.Whole))
-            music.playTone(262, music.beat(BeatFraction.Whole))
-            if (Math.round(Math.map(input.lightLevel(), 0, 255, 0, 2)) == 0) {
-                music.playTone(330, music.beat(BeatFraction.Whole))
-            } else if (Math.round(Math.map(input.lightLevel(), 0, 255, 0, 2)) == 1) {
-                music.playTone(392, music.beat(BeatFraction.Whole))
-            } else if (Math.round(Math.map(input.lightLevel(), 0, 255, 0, 2)) == 2) {
-                music.playTone(523, music.beat(BeatFraction.Whole))
-            }
-            music.playTone(262, music.beat(BeatFraction.Whole))
-        }
-    }
-}
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(120)
     basic.pause(1000)
@@ -28,7 +11,6 @@ input.onButtonPressed(Button.AB, function () {
     debug_mode = true
     müsig = true
     bpm_erhalten = 100
-    Musik(bpm_erhalten)
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "S") {
@@ -49,5 +31,17 @@ debug_mode = false
 radio.setGroup(28)
 radio.setTransmitPower(4)
 basic.forever(function () {
-    Musik(bpm_erhalten)
+    while (müsig) {
+        music.setTempo(bpm_erhalten)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        if (Math.round(Math.map(input.lightLevel(), 0, 255, 0, 2)) == 0) {
+            music.playTone(330, music.beat(BeatFraction.Whole))
+        } else if (Math.round(Math.map(input.lightLevel(), 0, 255, 0, 2)) == 1) {
+            music.playTone(392, music.beat(BeatFraction.Whole))
+        } else if (Math.round(Math.map(input.lightLevel(), 0, 255, 0, 2)) == 2) {
+            music.playTone(523, music.beat(BeatFraction.Whole))
+        }
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
 })
